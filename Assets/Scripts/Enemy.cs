059ef72b3dd74e7a059e737d3cceaf212c4e674f
Dane B. <dbake106@students.kennesaw.edu>
2025-10-08 19:56:10 -0400

    Basic Enemy Movement
    They don't have a is_on_wall() here :(

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Unity.Hierarchy;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -6,21 +8,40 @@ public class enemy : MonoBehaviour
 {
     [Header("Enemy Attributes")]
     [SerializeField] private float speed;
-    private int direction = -1;
-    private Transform en_transform;
+    [SerializeField] private List<GameObject> destinationPoints = new List<GameObject>();
 
-    private void Start() => en_transform = transform;
+    private Rigidbody2D rb;
+    private Transform destination;
+    private int destinationID = 0;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        destination = destinationPoints[destinationID].transform;
+    }
 
     private void FixedUpdate()
     {
-        en_transform.position = new Vector2(en_transform.position.x + speed * direction *Time.deltaTime, en_transform.position.y);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.GetComponent<TilemapCollider2D>() != null)
+        Vector2 point = destination.position - transform.position;
+        if (destinationID == 0)
         {
-            direction *= -1;
+            rb.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+        if (Vector2.Distance(transform.position, destination.position) < 0.5f)
+        {
+            if (destinationID == destinationPoints.Count - 1)
+            {
+                destinationID = 0;
+            }
+            else
+            {
+                destinationID += 1;
+            }
+            destination = destinationPoints[destinationID].transform;
         }
     }
 }
