2170fb180cdf47fc2d08e45a60b8f8bd959ff0ea
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-10-08 19:35:53 -0400

    created script to animate transition and hooked it into the player input component

--- Assets/Scripts/RingSwitcherController.cs
+++ Assets/Scripts/RingSwitcherController.cs
@@ -1,16 +1,48 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RingSwitcherController : MonoBehaviour
 {
+
+    public GameObject player;
+    Material _material;
+    Boolean cool = false;
+    Boolean moving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _material = GetComponent<Material>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving)
+        {
+            float percent = _material.GetFloat("Percent");
+
+            if (cool && percent < 1)
+            {
+                _material.SetFloat("Percent", percent + 0.01f);
+            }
+            else if (!cool && percent > 0)
+            {
+                _material.SetFloat("Percent", percent - 0.01f);
+            }
+            else
+            {
+                moving = false;
+            }
+        }
+    }
+
+    public void SwapSides(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            cool = !cool;
+            moving = true;
+        }
     }
 }
