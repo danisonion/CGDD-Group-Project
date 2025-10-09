7f020c33f0f9e50a2e52efa7f06a6d8a4ea91beb
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-10-08 20:00:06 -0400

    Made RingSwitcher appear and disappear
    TODO: Make this overtop of the player using screenspace stuff

--- Assets/Scripts/RingSwitcherController.cs
+++ Assets/Scripts/RingSwitcherController.cs
@@ -1,18 +1,22 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class RingSwitcherController : MonoBehaviour
 {
 
     public GameObject player;
     Material _material;
+    Image _image;
     Boolean cool = false;
     Boolean moving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _material = GetComponent<Material>();
+        _image = GetComponent<Image>();
+        _material = _image.material;
+        _image.enabled = false;
     }
 
     // Update is called once per frame
@@ -20,21 +24,27 @@ public class RingSwitcherController : MonoBehaviour
     {
         if (moving)
         {
-            float percent = _material.GetFloat("Percent");
+            float percent = _material.GetFloat("_Percent");
 
             if (cool && percent < 1)
             {
-                _material.SetFloat("Percent", percent + 0.01f);
+                _material.SetFloat("_Percent", percent + Time.deltaTime * 3);
             }
             else if (!cool && percent > 0)
             {
-                _material.SetFloat("Percent", percent - 0.01f);
+                _material.SetFloat("_Percent", percent - Time.deltaTime * 3);
             }
             else
             {
                 moving = false;
+                Invoke(nameof(hideImage), 0.5f);
             }
         }
+    }
+
+    void hideImage()
+    {
+        _image.enabled = false;
     }
 
     public void SwapSides(InputAction.CallbackContext context)
@@ -43,6 +53,8 @@ public class RingSwitcherController : MonoBehaviour
         {
             cool = !cool;
             moving = true;
+            _image.enabled = true;
+            CancelInvoke(nameof(hideImage));
         }
     }
 }
