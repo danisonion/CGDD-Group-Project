6ad164316e908bd8b3d4c2dda8e07bee7e20d2e7
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-11-05 19:14:14 -0500

    create AbilitySet abstract class

---
+++ Assets/Scripts/AbilitySetBase.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+abstract public class AbilitySetBase : MonoBehaviour
+{
+    //region We probably don't need this, but I'm keeping it here just in case
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //endregion
+
+    abstract public void ability1(GameObject player);
+
+    abstract public void ability2(GameObject player);
+
+    abstract public void ability3(GameObject player);
+}
