c604c0c17c3b0714b3f2606e1705967bada56631
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-11-05 21:14:05 -0500

    Created Cold and Warm Ability Set files

--- Assets/Scripts/AbilitySetBase.cs
+++ Assets/Scripts/AbilitySetBase.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 abstract public class AbilitySetBase : MonoBehaviour
 {
+    public bool usingAbility1;
+    public bool usingAbility2;
+    public bool usingAbility3;
+
     //region We probably don't need this, but I'm keeping it here just in case
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
