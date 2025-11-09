d28171f4f507e1a0a4c892b470228b9ea1212faa
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-11-08 22:28:37 -0500

    create Empty Ability classes

---
+++ Assets/Scripts/HealingOrangeAbility.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HealingOrangeAbility : AbilityBase
+{
+    public HealingOrangeAbility(GameObject player, Timer cooldown) : base(player, cooldown)
+    {
+    }
+
+    public override void Ability()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
